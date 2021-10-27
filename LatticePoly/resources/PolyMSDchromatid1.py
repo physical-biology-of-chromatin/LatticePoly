9ae3aed697666f150b7df4a4e35b332a945af803
DarioDasaro <dariod2296@gmail.com>
2021-10-27 17:43:31 +0200

    polyMSDchromatid

--- LatticePoly/resources/PolyMSDchromatid1.py
+++ LatticePoly/resources/PolyMSDchromatid1.py
@@ -19,7 +19,7 @@ from vtkReader import vtkReader
 class PolyMSD():
 
 	def __init__(self, outputDir, initFrame):
-		self.Nchain+=1
+	self.Nchain=0
 		self.reader = vtkReader(outputDir, initFrame, readLiq=False, readPoly=True)
 		for t in range(self.reader.nTad):
 				if(self.reader.Status[t]==-1 or self.reader.Status[t]==0):
@@ -44,7 +44,7 @@ class PolyMSD():
 			posHist = self.ReadHist()
 			np.save("posHist",posHist)
 
-			for idxTad in range(self.Nchain):
+			for idxTad in range(self.reader.nTad):
 				if self.reader.polyType[idxTad] == 1:
 					self.cumulDistHet += msdFFT(posHist[:, idxTad])
 				else:
