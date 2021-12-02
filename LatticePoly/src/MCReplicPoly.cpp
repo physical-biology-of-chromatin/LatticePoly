fddc2902f83af725d823080fac2559906eb47611
DarioDasaro <dariod2296@gmail.com>
2021-12-02 17:26:29 +0100

    trial different repl params

--- LatticePoly/src/MCReplicPoly.cpp
+++ LatticePoly/src/MCReplicPoly.cpp
@@ -74,6 +74,7 @@ void MCReplicPoly::TrialMove(double* dE)
 void MCReplicPoly::OriginMove()
 {
 	
+	/*
 	if(Ntad>=int(.95*Nchain+Nchain))
 	{
 		
@@ -90,7 +91,7 @@ void MCReplicPoly::OriginMove()
 		
 		exit(0);
 		
-	}
+	}*/
 
 	if ( origins.size() > 0 and MCsteps> (Nrelax)*Ninter )
 	{
