04d4875729c858f491e8e98b4fe7fbec87a1a19d
DarioDasaro <dariod2296@gmail.com>
2022-01-18 17:41:41 +0100

    chromatid gyration check

--- LatticePoly/src/MCReplicPoly.cpp
+++ LatticePoly/src/MCReplicPoly.cpp
@@ -65,7 +65,7 @@ void MCReplicPoly::Init(int Ninit)
 	
 	
 
-	//origins={90};
+	origins={90};
 	
 	
 
@@ -144,7 +144,7 @@ void MCReplicPoly::ForkMove()
 		{
 			MCTad* fork = activeForks[i];
 			double rndReplic = lat->rngDistrib(lat->rngEngine);
-			if ( rndReplic < replicRate and fork->isFork())
+			if ( rndReplic < replicRate and fork->isFork() and (Ntad<Nchain+50))
 			{
 				Replicate(fork);
 			}
