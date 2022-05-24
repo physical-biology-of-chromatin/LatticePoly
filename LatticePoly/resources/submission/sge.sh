7af7cef77d2dfcc49a8deec7a8544ac7179f46de
DarioDasaro <dariod2296@gmail.com>
2022-05-24 18:24:28 +0200

    corrected sge.h

--- LatticePoly/resources/submission/sge.sh
+++ LatticePoly/resources/submission/sge.sh
@@ -42,26 +42,30 @@ DIRSUB="s|\(outputDir[[:space:]]*=[[:space:]]*\)\(.*;\)|\1${TMPDIR} ;|;"
 
 # Copy input configuration file to output directory, substituting paths and parameter values
 sed -e "${DIRSUB}" < data/input.cfg > ${TMPDIR}/input.cfg
+sed -e "${DIRSUB}" < data/box.vtp > ${TMPDIR}/box.vtp
+sed -e "${DIRSUB}" < data/poly100001.vtp > ${TMPDIR}/poly100001.vtp
+
+
 
 # Run
 ./${EXEC} ${TMPDIR}/input.cfg > ${TMPDIR}/log.out
 
 # analysis
-#python3 resources/Yeast_distance_1.py ${OUTDIR}  100001  >> ${OUTDIR}/process.out
-#python3 resources/Yeast_distance_2.py ${OUTDIR}  100001  >> ${OUTDIR}/process.out
-#python3 resources/Yeast_distance_3.py ${OUTDIR}  100001  >> ${OUTDIR}/process.out
-#python3 resources/Forksnumber.py ${OUTDIR}  100001  >> ${OUTDIR}/process.out
-#python3 resources/MonomerDist.py ${OUTDIR}  100201 0 872  >> ${OUTDIR}/process.out #after G1
-#python3 resources/MonomerDist.py ${OUTDIR}  100334 0 872  >> ${OUTDIR}/process.out #1min
-#python3 resources/MonomerDist.py ${OUTDIR}  100401 0 872  >> ${OUTDIR}/process.out #3min
-#python3 resources/MonomerDist.py ${OUTDIR}  100501 0 872  >> ${OUTDIR}/process.out #6min
-#python3 resources/MonomerDist.py ${OUTDIR}  100601 0 872  >> ${OUTDIR}/process.out #9min
-#python3 resources/MonomerDist.py ${OUTDIR}  100801 0 872  >> ${OUTDIR}/process.out #15min
-#python3 resources/MonomerDist.py ${OUTDIR}  101101 0 872  >> ${OUTDIR}/process.out #24min
+python3 resources/Yeast_distance_1.py ${TMPDIR}  100101
+python3 resources/Yeast_distance_2.py ${TMPDIR}  100101
+python3 resources/Yeast_distance_3.py ${TMPDIR}  100101
+python3 resources/Forksnumber.py ${TMPDIR}  100101
+python3 resources/MonomerDist_HiC.py ${TMPDIR}  100201 0 872   #after G1
+python3 resources/MonomerDist_HiC.py ${TMPDIR}  100334 0 872   #1min
+python3 resources/MonomerDist_HiC.py${TMPDIR}  100401 0 872   #3min
+python3 resources/MonomerDist_HiC.py ${TMPDIR}  100501 0 872   #6min
+python3 resources/MonomerDist_HiC.py ${TMPDIR}  100601 0 872  #9min
+python3 resources/MonomerDist_HiC.py ${TMPDIR}  100801 0 872   #15min
+python3 resources/MonomerDist_HiC.py ${TMPDIR}  101101 0 872   #24min
 
 
 
-python3 resources/Forksnumber.py ${OUTDIR}  100801  >> ${OUTDIR}/process.out
+#python3 resources/Forksnumber.py ${TMPDIR}  100801
 
 
 # Move SGE output files to data directory
