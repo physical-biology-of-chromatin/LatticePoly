f20ce8441be3d67e0b4f3008f339e9e096642c9e
DarioDasaro <dariod2296@gmail.com>
2022-05-30 16:34:03 +0200

    new potential for sister replisome

--- LatticePoly/resources/process_matrices.py
+++ LatticePoly/resources/process_matrices.py
@@ -18,7 +18,8 @@ import networkx as nx
 import time
 import json
 from numpy import nanmean
-
+import numba
+from numba import jit, int32
 
 
 outputDir = sys.argv[1]
@@ -28,14 +29,18 @@ timeframe = sys.argv[2]
 cis=[]
 trans=[]
 allint=[]
-for folder in os.listdir(outputDir):
-	if(folder.endswith('.gz')==False):
-		for file_name in os.listdir(outputDir+'/'+folder):
-			if file_name.startswith(str(timeframe)):
-				file_path = os.path.join(outputDir+'/'+folder, file_name)
-				cis.append(np.loadtxt(file_path))
-				break;
+
+@numba.jit()
+def merge_matrices(outputDir,timeframe)
+	for folder in os.listdir(outputDir):
+		if(folder.endswith('.gz')==False):
+			for file_name in os.listdir(outputDir+'/'+folder):
+				if file_name.startswith(str(timeframe)):
+					file_path = os.path.join(outputDir+'/'+folder, file_name)
+					cis.append(np.loadtxt(file_path))
+					break;
 
 
-finalcis=nanmean(cis,axis=0)
-np.savetxt(outputDir+"/"+str(timeframe)+"finalcis.res", finalcis)
+	finalcis=nanmean(cis,axis=0)
+	return finalcis
+np.savetxt(outputDir+"/"+str(timeframe)+"finalcis.res", merge_matrices(outputDir,timeframe))
